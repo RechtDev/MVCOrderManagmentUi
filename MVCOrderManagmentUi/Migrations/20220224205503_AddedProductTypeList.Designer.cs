2d04c6fed301675a5e80a219d57209be95e8fb0f
Naziyah <31572120+Yourconnect@users.noreply.github.com>
2022-02-24 16:41:53 -0600

    Added enhancement issue #1 (#5)
    * issue #1; Centered form and heading added two icons
    
    * finished issue #1 Added drop down list for product types
    
    * Delete 20220224205503_AddedProductTypeList.cs
    
    Deleted extra migration. Looks good!
    
    Co-authored-by: Domonic Francis <32023240+RechtDev@users.noreply.github.com>

--- MVCOrderManagmentUi/Migrations/20220224205503_AddedProductTypeList.Designer.cs
+++ MVCOrderManagmentUi/Migrations/20220224205503_AddedProductTypeList.Designer.cs
@@ -4,14 +4,16 @@ using MVCOrderManagmentUi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVCOrderManagmentUi.Migrations
 {
     [DbContext(typeof(GSparkShopDBContext))]
-    partial class GSparkShopDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220224205503_AddedProductTypeList")]
+    partial class AddedProductTypeList
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
