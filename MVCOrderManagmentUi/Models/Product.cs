4969389de0aa6e2ff89c4a82ac2dcd10d06a23d2
Domonic Francis <rechtproductions@gmail.com>
2022-02-28 14:35:11 -0500

    Added Valdation for Forms

--- MVCOrderManagmentUi/Models/Product.cs
+++ MVCOrderManagmentUi/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,6 +9,7 @@ namespace MVCOrderManagmentUi.Models
     public partial class Product
     {
         public int ProdId { get; set; }
+        [Required]
         public string ProdName { get; set; }
         public decimal ProdPrice { get; set; }
         public string ProdType { get; set; }
