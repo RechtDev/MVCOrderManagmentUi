c49873d3a88cc3eb077c586ce6e4c0dc0132cf63
Naziyah <ziyahfolmar@gmail.com>
2022-02-28 14:00:46 -0500

    Fixed about h1 tags, gave them ids add more validation on create new product page

--- MVCOrderManagmentUi/Models/Product.cs
+++ MVCOrderManagmentUi/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,8 +9,12 @@ namespace MVCOrderManagmentUi.Models
     public partial class Product
     {
         public int ProdId { get; set; }
+        [Required(ErrorMessage = "Product name required")]
         public string ProdName { get; set; }
+        [Required(ErrorMessage = "Product price required")]
+        [DataType(DataType.Currency)]
         public decimal ProdPrice { get; set; }
+        [Required (ErrorMessage = "Product type required")]
         public string ProdType { get; set; }
     }
 }
